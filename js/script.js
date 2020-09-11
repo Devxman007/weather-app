852009c74e1088044c98ce7e5d8772c8eb07eb48
Oudani Achraf <achraf.oudani@gmail.com>
2020-09-11 22:26:53 +0200

    search with city name

--- js/script.js
+++ js/script.js
@@ -1,60 +1,71 @@
-let loc = document.getElementById("location");
-let tempIcon = document.getElementById("temp-icon");
-let tempValue = document.getElementById("temp-value");
-let climate = document.getElementById("climate");
-let iconFile ;
-window.addEventListener("load",() =>{
-let long;
-let lat;
-if(navigator.geolocation)
-{
-        navigator.geolocation.getCurrentPosition((position) =>{
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-        const proxy ="https://cors-anywhere.herokuapp.com/";
-        const api =`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=37ccf27648070a6eaf80d1dfedc0e9b3`;
-        fetch(api)
-        .then((response) =>{
-         return response.json();
-        })
-        .then(data => {
-         const {name} = data;
-         const {feels_like} = data.main;
-         const {country} = data.sys;
-         const {id , main, icon} = data.weather[0];
-         loc.textContent = name;
-         climate.textContent = main;
-         tempValue.textContent = Math.round(feels_like-273.15);
-         if(tempValue <250)
-         {
-             tempIcon.src ="./img/thunderstorm.svg";
-         }
-         else if (id <350)
-         {
-            tempIcon.src ="./img/rain.svg";
-        }
-         else if (id <550)
-        {
-             tempIcon.src ="./img/Sunny_Rain_Climate.svg";
-         }
-         else if (id <650)
-         {
-            tempIcon.src ="./img/Snow.svg";
-        }
-         else if (id <800)
-         {
-            tempIcon.src ="./img/Atmosphere.svg";
-        }
-         else if (id === 800)
-         {
-            tempIcon.src ="Hot_Sun_Day.svg";
-        }
-         else if (id > 800)
-         {
-            tempIcon.src ="./img/Sunny_Sun_Cloudy.svg";
-        }
-         console.log(data);
-        })
-    })
+const loc = document.getElementById("location");
+const tempIcon = document.getElementById("temp-icon");
+const tempValue = document.getElementById("temp-value");
+const climate = document.getElementById("climate");
+const searchValue = document.getElementById("searchValue");
+const searchButton = document.getElementById("searchBtn");
+
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.setItem("searchValue", searchValue.value);
+  if (searchValue.value === "") return;
+  showCityWeather(searchValue.value);
+});
+
+window.addEventListener("load", () => {
+  searchValue.value = localStorage.getItem("searchValue");
+  if (searchValue.value !== "") showCityWeather(searchValue.value);
+  else showCurrentLocationWeather();
+});
+
+function showCityWeather(cityName) {
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=37ccf27648070a6eaf80d1dfedc0e9b3`;
+  fetchApi(api);
 }
-})
+
+function showCurrentLocationWeather() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=37ccf27648070a6eaf80d1dfedc0e9b3`;
+      fetchApi(api);
+    });
+  }
+}
+
+function fetchApi(api) {
+  fetch(api)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      rendreView(data);
+    });
+}
+
+function rendreView(data) {
+  const { name } = data;
+  const { feels_like } = data.main;
+  const { country } = data.sys;
+  const { id, main, icon } = data.weather[0];
+  loc.textContent = name;
+  climate.textContent = main;
+  tempValue.textContent = feels_like;
+
+  if (feels_like < 250) {
+    tempIcon.src = "./img/thunderstorm.svg";
+  } else if (id < 350) {
+    tempIcon.src = "./img/rain.svg";
+  } else if (id < 550) {
+    tempIcon.src = "./img/Sunny_Rain_Climate.svg";
+  } else if (id < 650) {
+    tempIcon.src = "./img/Snow.svg";
+  } else if (id < 800) {
+    tempIcon.src = "./img/Atmosphere.svg";
+  } else if (id === 800) {
+    tempIcon.src = "Hot_Sun_Day.svg";
+  } else if (id > 800) {
+    tempIcon.src = "./img/Sunny_Sun_Cloudy.svg";
+  }
+}
--- sw.js
+++ sw.js
@@ -1,62 +1,84 @@
-'use strict';
+"use strict";
 
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
-
+const STATIC_CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
-  '/offline.html',
-  'css/style.css',
-  'js/script.js',
-  'index.js',
-  'index.html',
-  'img/crying-cloud.png'
-
+  "/",
+  "/offline.html",
+  "css/style.css",
+  "js/script.js",
+  "index.js",
+  "/index.html",
+  "img/crying-cloud.png",
+  "img/thunderstorm.svg",
+  "img/Atmosphere.svg",
+  "img/Hot_Sun_Day.svg",
+  "img/rain.svg",
+  "img/Sunny_Rain_Climate.svg",
+  "img/Sunny_Sun_Cloudy.svg",
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", (evt) => {
+  console.log("[ServiceWorker] Install");
 
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
+    caches.open(STATIC_CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching offline page");
       return cache.addAll(FILES_TO_CACHE);
     })
-);
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener("activate", (evt) => {
+  console.log("[ServiceWorker] Activate");
 
   evt.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== STATIC_CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
-  )
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
 
-  if (evt.request.mode !== 'navigate') {
-
-    return;
-  }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
+  if (evt.request.url.includes("openweathermap")) {
+    console.log("[Service Worker] Fetch (data)", evt.request.url);
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+          .then((response) => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
           })
-  );
+          .catch((err) => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      })
+    );
+  } else {
+    evt.respondWith(
+      fetch(evt.request).catch(() => {
+        return caches.open(STATIC_CACHE_NAME).then((cache) => {
+          return cache.match(evt.request).then((response) => {
+            return response || cache.match("/offline.html")
+          });
+        });
+      })
+    );
+  }
 });
-
