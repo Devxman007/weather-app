ab30a78be74d1bf487bae266c408d8fa9281845e
Oudani Achraf <achraf.oudani@gmail.com>
2020-09-11 16:26:00 +0200

    update service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,35 @@
+'use strict';
+
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+
 const FILES_TO_CACHE = [
-    '/offline.html',
-  ];
+  '/offline.html',
+  'css/style.css',
+  'js/script.js',
+  'index.js',
+  'index.html',
+  'img/crying-cloud.png'
+
+];
+
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
 );
-evt.waitUntil(
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+
+  evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
@@ -16,4 +38,25 @@ evt.waitUntil(
         }
       }));
     })
-)
+  )
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+
+  if (evt.request.mode !== 'navigate') {
+
+    return;
+  }
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('offline.html');
+                });
+          })
+  );
+});
+
